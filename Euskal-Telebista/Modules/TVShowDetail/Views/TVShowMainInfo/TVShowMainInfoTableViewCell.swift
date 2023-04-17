d07fd8f185c5246eaff6fe4b3752734fc3a20680
Aitor Zubizarreta <aitorzubizarreta@yahoo.es>
2023-04-17 21:05:43 +0200

    Update the cells "TVShowMainInfoTableViewCell" and "TVShowPlaylistInfoTableViewCell" to set the selection style to none.

--- Euskal-Telebista/Modules/TVShowDetail/Views/TVShowMainInfo/TVShowMainInfoTableViewCell.swift
+++ Euskal-Telebista/Modules/TVShowDetail/Views/TVShowMainInfo/TVShowMainInfoTableViewCell.swift
@@ -40,13 +40,18 @@ class TVShowMainInfoTableViewCell: UITableViewCell {
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        
+        setupView()
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
+    }
+    
+    private func setupView() {
+        selectionStyle = .none
     }
     
 }
